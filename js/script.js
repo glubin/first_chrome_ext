37ffd8fdd3393cf82696d84d7379908ca479d5b5
Greg Lubin <greg@greglubin.com>
2015-03-09 15:34:31 -0400

    delete excess

--- js/script.js
+++ js/script.js
@@ -30,11 +30,13 @@ $( document ).ready(function() {
 		}
 
 		$('#my_form').keydown(function() {
-var key = e.which;
-if (key == 13) {
-// As ASCII code for ENTER key is "13"
-$('#my_form').submit(); // Submit form code
-}
+			var key = e.which;
+			if (key == 13) {
+
+		$('#my_form').submit(); {} // Submit form code
+		//
+		}
+	}
 });
 	});
 
